e6f03ef1cad1eb43cf51962c0b249329cb7591e5
Cova <Cova@138.4.70.109>
2017-09-25 18:01:58 +0200

    Create C32Test

---
+++ src/test/java/PIndividual/C32Test.java
@@ -0,0 +1,13 @@
+package PIndividual;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class C32Test {
+
+    @Test
+    public void testM2() {
+        assertEquals("C32: mA",new C32().mA());
+    }
+}
