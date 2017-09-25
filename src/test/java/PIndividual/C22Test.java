9052da46609bff3f1405b628c1e24c08310acc0c
Cova <Cova@138.4.70.109>
2017-09-25 17:23:20 +0200

    Create C22Test

---
+++ src/test/java/PIndividual/C22Test.java
@@ -0,0 +1,13 @@
+package PIndividual;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class C22Test {
+
+    @Test
+    public void testA() {
+        assertEquals("C22: mA",new C22().mA());
+    }
+}
