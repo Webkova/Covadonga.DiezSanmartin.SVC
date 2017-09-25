ba82e8944894abd32a51d69160d9e45ad756ebf8
Cova <Cova@138.4.70.109>
2017-09-25 16:57:29 +0200

    Configuration changes

---
+++ scripts/package.bat
@@ -0,0 +1,18 @@
+@echo off
+set workspace=C:\Users\Cova\Documents\MIW\MIW_WS_FORJA\Cova.Diez.SVC
+::Las variables de entorno se pueden configurar directamente en Windows cuando se tienen los permisos necesarios
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
+echo -----------------------------------------
+echo . (C) MIW - UPM
+echo -----------------------------------------
+echo .
+echo Workspace --- %workspace%
+echo JAVA_HOME --- %JAVA_HOME%
+echo M2_HOME   --- %M2_HOME%
+echo .
+cd %workspace%
+echo ============ mvn clean test --settings settings.xml  (profile: develop) =====================================
+echo .
+call mvn clean test -Denvironment.type=develop package -Denvironment.type=preproduction --settings settings.xml 
+pause
+
