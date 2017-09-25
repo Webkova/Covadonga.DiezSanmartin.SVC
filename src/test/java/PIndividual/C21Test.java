c798ad970e26d4ff4cdc0a1aa9433377320737fc
Cova <Cova@138.4.70.109>
2017-09-25 17:20:13 +0200

    Create C21Test

---
+++ src/test/java/PIndividual/C21Test.java
@@ -0,0 +1,18 @@
+package PIndividual;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class C21Test {
+
+    @Test
+    public void testM1() {
+        assertEquals("C21: m1",new C21().m1());
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals("C21: m2",new C21().m2());
+    }
+}
