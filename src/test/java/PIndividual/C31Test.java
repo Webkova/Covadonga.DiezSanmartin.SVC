b8bf99374f0219b095d327ce7deea82cce510522
Cova <Cova@138.4.70.109>
2017-09-25 17:55:11 +0200

    Create C31Test

---
+++ src/test/java/PIndividual/C31Test.java
@@ -0,0 +1,18 @@
+package PIndividual;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class C31Test {
+
+    @Test
+    public void testM1() {
+        assertEquals("C31: m1",new C31().m1());
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals("C31: m2",new C31().m2());
+    }
+}
